0e52fe76a2a84b3546d3160c0d8617c50a6103bb
foureverhh <foureverhh@gmail.com>
2018-05-28 10:06:26 +0200

    Initial commit

---
+++ src/main/java/com/springtestdatabase180528/JsondbApplication.java
@@ -0,0 +1,12 @@
+package com.springtestdatabase180528;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsondbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsondbApplication.class, args);
+	}
+}
